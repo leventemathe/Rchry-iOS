2d2e385886749834ef696a71c96410582686762d
Levente Máthé <leviouss@gmail.com>
2017-12-06 16:59:12 +0100

    Added global localizations for strings.

--- Rchry/View/Targets/TargetsVC.swift
+++ Rchry/View/Targets/TargetsVC.swift
@@ -10,9 +10,4 @@ import UIKit
 
 class TargetsVC: UIViewController {
 
-    override func viewDidAppear(_ animated: Bool) {
-        MessageAlertModalVC.present(withTitle: NSLocalizedString("TargetsOKModalTitle", comment: "The title of the modal in targets"),
-                                    withMessage: NSLocalizedString("TargetsOkModalMessage", comment: "The message of the modal in targets"),
-                                    fromVC: self)
-    }
 }
